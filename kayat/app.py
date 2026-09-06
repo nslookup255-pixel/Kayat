1d7b5da624f9095bd8ea174b162555e5b4f29a2b
Ha Yul Noh <nslookup.255@gmail.com>
2026-09-06 16:02:13 +0800

    Update app integration

--- kayat/app.py
+++ kayat/app.py
@@ -1,12 +1,9 @@
-# import modules
 from .window import Window
 
 class App:
     def __init__(self, title="Kayat App"):
         self.title = title
-        self.window = Window(title=title)
+        self.window = Window(title)
 
     def run(self):
-        print(f"Running {self.title}...")
         self.window.show()
-        # Add your application logic here
